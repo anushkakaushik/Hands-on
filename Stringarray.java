1f0347e9e7c95c31a90c15f1eff467908d1d4965
Archana Jha <114074480+ArchanaaJha@users.noreply.github.com>
2022-10-19 16:28:52 +0530

    Add files via upload

---
+++ Stringarray.java
@@ -0,0 +1,21 @@
+package com.src;
+
+public class Stringarray {
+	
+	public static void main(String[] args) {
+		// Declaring and instantiating the array
+		String a[] = new String[3];
+		// initialization
+		a[0]="Krishna";
+		a[1]="Gopi";
+		a[2]="Radha";
+		// traversing the array
+	for(int i=0;i<a.length;i++) {
+		System.out.println(a[i]);
+	}
+	}
+	
+
+}
+
+
